42362093e110e7dfc3d77b14c2bcffae25ad3eda
husain <Hussain.folath@gmail.com>
2025-07-11 15:24:14 +0300

    made a function (createCard(cardId)) that creates a card in the DOM with the card ID (updated battleScript.js)

--- js/battleScript.js
+++ js/battleScript.js
@@ -1,3 +1,10 @@
+// const { createElement } = require("react");
+
+//list of available cards
+const cards =
+[
+{id:1,name: "Enchanting blood",cost:3, type:"Power", effect: "nextDamage*2", description:"doubles the next instance of damage.",  img: "url"}
+]
 console.log("Bye!")
 //make the button send the player to the battle page
 const quitBtn= document.getElementById("quitBtn");
@@ -6,8 +13,39 @@ quitBtn.addEventListener("click", ()=>
     window.location.href="index.html";
 })
 
-const cards =
-[
-{ID:1,name: "Enchanting blood",cost:3, type:"Power", effect: "nextDamage*2", description:"doubles the next instance of damage.",  img: "url"}
-]
 console.log(cards)
+ function createCard(cardId)
+ {
+    const newCardInfo=cards.find(card=>card.id===cardId)
+
+    const newCard= document.createElement("div")
+    newCard.classList.add("card")
+
+    const newCardName=document.createElement("div")
+    newCardName.classList.add("cardName")
+    newCardName.textContent=newCardInfo.name
+
+    const newCardMid=document.createElement("div")
+    newCardMid.classList.add("cardMid")
+
+    const newCardImage=document.createElement("div")
+    newCardImage.classList.add("cardImage")
+    newCardImage.textContent=newCardInfo.img
+
+    const newCardCost=document.createElement("div")
+    newCardCost.classList.add("cardCost")
+    newCardCost.textContent=newCardInfo.cost
+
+    const newCardDescription=document.createElement("div")
+    newCardDescription.classList.add("cardDescription")
+    newCardDescription.textContent=newCardInfo.description
+
+    newCardMid.append(newCardCost,newCardImage)
+
+    newCard.append(newCardName,newCardMid,newCardDescription)
+
+    document.body.appendChild(newCard)
+
+    // console.log(newCard)
+ }
+ createCard(1)
